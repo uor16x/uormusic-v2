53af2c2dfa4cc5570018acf513e747642ef7ee23
uor16x <u0r.home@gmail.com>
2021-03-22 22:40:29 +0200

    baseURL from env; Mobile regex fix;

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-	baseURL: `http://localhost:8095/`,
+	baseURL: process.env.REACT_APP_BASE_URL,
 	withCredentials: true,
 	headers: {
 		'Accept': 'application/json',
